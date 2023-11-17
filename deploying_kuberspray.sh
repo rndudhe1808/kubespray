d0676b622b1787833a06f5e6cfedbb4cc4435ae7
Rameshwar Dudhe <141106013+rndudhe1808@users.noreply.github.com>
2023-11-17 16:07:45 +0530

    Update deploying_kuberspray.sh

--- deploying_kuberspray.sh
+++ deploying_kuberspray.sh
@@ -1,10 +1,10 @@
-1.install python and ansible compatbile and lates version
-2.generate all ssh keys of all host 
-3.declare hostname accordingly
-4.on master  collect all pvt ip's of ec2 and decalre in /etc/hosts on each machine
-5.clone the git repo
-6.install dependacies from file
-7.decalare ip's 
-8.verfiy yaml
-9.start build
-10.Done 
+#1.install python and ansible compatbile and lates version
+#2.generate all ssh keys of all host 
+#3.declare hostname accordingly
+#4.on master  collect all pvt ip's of ec2 and decalre in /etc/hosts on each machine
+#5.clone the git repo
+#6.install dependacies from file
+#7.decalare ip's 
+#8.verfiy yaml
+#9.start build
+#10.Done 
